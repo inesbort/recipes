5ee6da3512bfaa542a4963d7316d65a359c970d0
inesbort <ixb303@student.bham.ac.uk>
2024-12-02 16:55:12 +0000

    Updated recipe class
    Made all attributes private rather than public static, and introduced name attribute and a few getter and setter methods

--- Recipe.java
+++ Recipe.java
@@ -1,82 +1,103 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Recipe {
-    public static List<String> ingredients;
 
-    public static List<String[]> ingredientWithAmount;
+    private List<String> ingredients= new ArrayList<>();
+    private List<String[]> ingredientWithAmount= new ArrayList<>();
+    private int weight;
+    private int id;
+    private String description;
+    private String prep_time;
+    private String cook_time;
+    private String image;
 
-    public static int weight;
-    public static int id;
-    public static String description;
-    public static String prep_time;
-    public static String cook_time;
-    public static String image;
+    private String name;
+    static List<String> steps = new ArrayList<>();
 
-    public static List<String> steps;
+    public Recipe(int id_,String name_,String description_,String prep_time_,String cook_time_){
+        id=id_;
+        name=name_;
+        description=description_;
+        prep_time=prep_time_;
+        cook_time=cook_time_;
+
+    }
+
+    public void setName(String name_) {
+        name = name_;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public void addIngredient(String ingredient){
         ingredients.add(ingredient);
     }
 
-    public static List<String> getIngredients() {
+    public List<String> getIngredients() {
         return ingredients;
     }
 
-    public static int getWeight() {
+    public List<String[]> getIngredientWithAmount() {
+        return ingredientWithAmount;
+    }
+
+    public List<String> getSteps() {
+        return steps;
+    }
+
+    public int getWeight() {
         return weight;
     }
 
-    public static void setWeight(int weight) {
-        Recipe.weight = weight;
+    public void setWeight(int weight_) {
+        weight = weight_;
     }
-    public static int getId(){
+    public int getId(){
         return  id;
     }
-    public static String setDescription(){
+    public String setDescription(){
         return description;
     }
 
-    public static String getPrep_time(){
+    public  String getPrep_time(){
         return prep_time;
     }
 
-    public static String getCook_time() {
+    public String getCook_time() {
         return cook_time;
     }
 
-    public static void setId(int id) {
-        Recipe.id = id;
+    public void setId(int id_) {
+        id = id_;
     }
 
-    public static String getDescription() {
+    public String getDescription() {
         return description;
     }
 
-    public static String getImage() {
+    public String getImage() {
         return image;
     }
 
-    public static void setCook_time(String cook_time) {
-        Recipe.cook_time = cook_time;
+    public void setCook_time(String cook_time_) {cook_time = cook_time_;}
+
+    public void setDescription(String description_) {
+    description = description_;
     }
 
-    public static void setDescription(String description) {
-        Recipe.description = description;
+    public void setImage(String image_) {image = image_;}
+
+    public  void setPrep_time(String prep_time_) {
+        prep_time = prep_time_;
     }
 
-    public static void setImage(String image) {
-        Recipe.image = image;
-    }
-
-    public static void setPrep_time(String prep_time) {
-        Recipe.prep_time = prep_time;
-    }
-
-    public static void addStep(String step){
+    public void addStep(String step){
         steps.add(step);
     }
-    public static void addDetailIngredient(String amount,String unit,String ingredient){
-
+    public void addDetailIngredient(String amount,String unit,String ingredient){
         ingredientWithAmount.add(new String[]{amount,unit,ingredient});
     }
 
